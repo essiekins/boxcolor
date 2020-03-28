de96464d7e7b29d48c55b0c1e971ddaec0ad997b
Your Name <Student_MA117@Student-MA117.local>
2020-03-28 15:56:33 -0400

    fixing counter, if doesn't work delete later

--- src/App.js
+++ src/App.js
@@ -28,23 +28,28 @@ color: this.state.color,
 })
 // console.log(colors[i]);
 
+//color changer clicker
 document.getElementsByClassName('toggleBox')[0].style.backgroundColor = colors[i];
 
+//clicker count
+document.getElementsByClassName('toggleBox').text.clickCount = clickCount;
 
 } /*end of colorCycle */
-
-
 
 render () {
   return (
     <div className="App">
       <div className="title"> (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ Hello Disco Boxes ✧ﾟ･:*. </div>
-     <div className="boxHolders">
-      <div className="toggleBox" onClick={this.colorCycle}>
-       like being clicked
+      <h1>Click this little box to see him rave!</h1>
+     <div className="boxHolders"> 
+      <div className="timesClicked" onClick={this.clickCount}>
+        I've been clicked this many times:
+        <p>{clickCount}</p>
+      </div>
        
-      
-      
+      <div className="toggleBox" onClick={this.colorCycle}>
+      〜(^∇^〜）<br></br>♩ ♪ ♫ ♬ <br></br>（〜^∇^)〜
+
        </div> {/*end of box holder */}
       </div>
       
