ea8356ea5d73f20bcdc9b5e3917471f7142a4f55
Your Name <Student_MA117@Student-MA117.local>
2020-03-28 02:02:32 -0400

    created state, need to fix syntax error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+export default class App extends Component {
+  constructor (props) {
+    super(props);
+  
+  /* need a starting state with a starting color */
+  /* also need a clicker counter, also update my state as many time as it clicks */
+  this.state= {
+    color:'pink',
+    clickCount= 0, 
+  }
+}
   return (
     <div className="App">
-      
+      <h1> Hello Disco Boxes ^u^</h1>
      
     </div>
   );
 }
+
 
 export default App;
